5f569fefd06d312b9175f5b5eaa04391f2ce0ed3
gopal2k6 <gopal2k6.1983@gmail.com>
2022-09-05 11:29:18 +0530

    1 file modified

--- App.tsx
+++ App.tsx
@@ -1,11 +1,25 @@
 import * as React from 'react';
 import './style.css';
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { valuetxt: 'Please enter the value' };
+    onchange = (event) => {
+      this.setState({ valuetxt: event.target.value });
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello StackBlitz!</h1>
+        <p>Start editing to see some magic happen :)</p>
+        <input type="text" onChange={this.onchange} />
+
+        <p>{this.state.valuetxt}</p>
+      </div>
+    );
+  }
 }
